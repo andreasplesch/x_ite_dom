005f8910ce151e6177526f377948e010448f81a2
Andreas Plesch <andreasplesch@users.noreply.github.com>
2016-09-03 13:38:29 -0400

    Add files via upload

--- cobweb/APcobweb.1.26.js
+++ cobweb/APcobweb.1.26.js
@@ -18266,9 +18266,9 @@ function ($,
 
 			this .element = $("<div>") .addClass ("cobweb-browser-timing") .appendTo (this .getBrowser () .getElement () .find (".cobweb-surface"));
 			this .table   = $("<table></table>") .appendTo (this .element);
-			this .header  = $("<thead>") .append ($("<tr></tr>") .append ($("<th colspan=2></th>"))) .appendTo (this .table);
+			this .header  = $("<thead>") .append ($("<tr></tr>") .append ($("<th colspan='2'></th>"))) .appendTo (this .table);
 			this .body    = $("<tbody></tbody>") .appendTo (this .table);
-			this .footer  = $("<tfoot>") .append ($("<tr></tr>") .append ($("<td colspan=2></th>"))) .appendTo (this .table);
+			this .footer  = $("<tfoot>") .append ($("<tr></tr>") .append ($("<td colspan='2'></th>"))) .appendTo (this .table);
 			this .button  = $("<button>") .click (this .set_type__ .bind (this)) .appendTo (this .footer .find ("td"));
 			this .rows    = [ ];
 
