a4838ed59b7a07c60a70e9dc67fab508d9065c03
Andreas Plesch <andreasplesch@users.noreply.github.com>
2016-09-26 23:14:54 -0400

    Update cobweb_dom.js

--- cobweb/cobweb_dom.js
+++ cobweb/cobweb_dom.js
@@ -64,7 +64,7 @@ mybrowser.importDocument(myx3d); //now also attached x3dnode property to each no
 var loadsensor = mybrowser.getLoadSensor();
 //use isLoaded field to detect when all inlines are loaded
 //actually does not sense inlines by default
-loadsensor.watchList_.push(inline.x3dnode);
+//loadsensor.watchList_.push(inline.x3dnode);
 //var isLoadedField = loadsensor.getField("isLoaded");
 isLoadedField.addFieldCallback("isLoaded", appendInternalDoms);
 
