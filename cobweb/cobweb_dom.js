8530fbd3a03e52386c3966f011e3ba22e291f1bb
Andreas Plesch <andreasplesch@users.noreply.github.com>
2016-10-04 23:07:40 -0400

    Update cobweb_dom.js

--- cobweb/cobweb_dom.js
+++ cobweb/cobweb_dom.js
@@ -36,7 +36,7 @@ function findAncestor (element, name) {
 		
 function processInlineDOM (element) {
 	if (element.x3dnode == undefined) { return; }// check for USE inline
-	var callback = appendInlineDOM.bind(this, element, wList.getValue());
+	var callback = appendInlineDOM.bind(this, element, wList.getValue().slice());
 	isLoadedField.addFieldCallback("loaded"+element.x3dnode.getId(), callback) 
 	//just add to watchlist ?
 	wList.setValue(wList.getValue().push(element.x3dnode)); // will trigger isLoaded event for this inline
