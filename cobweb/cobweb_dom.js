35655ec1f5185ccf940b962795b6b446015c6b4a
Andreas Plesch <andreasplesch@users.noreply.github.com>
2016-10-06 14:15:07 -0400

    Update cobweb_dom.js

--- cobweb/cobweb_dom.js
+++ cobweb/cobweb_dom.js
@@ -50,9 +50,9 @@ function appendInlineDOM (element, wListValue, isLoadedValue) {
 	isLoadedField.removeFieldCallback("loaded" + element.x3dnode.getId()) ;
 	//remove from watchlist
 	// restore passed, original watchlist
-	//wList.setValue(wListValue) ; // seems to work
+	wList.setValue(wListValue) ; // seems to work
 	// instead may need to look for element and remove it
-	wList.setValue(wList.getValue().filter(function(val) { return val !== element.x3dnode ; }));
+	//wList.setValue(wList.getValue().filter(function(val) { return val !== element.x3dnode ; })); // does not work
 	//any inlines in appended dom are picked up when Scene is a addedNode for Mutations
 	return;
 }
