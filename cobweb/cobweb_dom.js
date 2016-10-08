174cc00cb3310eae2a045ff1d7aef7cae0a3160f
Andreas Plesch <andreasplesch@users.noreply.github.com>
2016-10-08 15:34:32 -0400

    Update cobweb_dom.js

--- cobweb/cobweb_dom.js
+++ cobweb/cobweb_dom.js
@@ -35,7 +35,8 @@ function processAddedNode(addedEl, parser, mybrowser) {
 		mybrowser.currentScene.setup(); // consider a single setup() after all nodes are added
 	}
 	else if (typeof parent.x3dnode.addChildren === 'function') { // other way to check if grouping node ?
-		parent.x3dnode.addChildren(addedNode);
+		var addField = parent.x3dnode.getField('addChildren').getValue();
+		addField.setValue(addedNode);
 	}
 	else { console.log('do not know how to add: ' + addedEl.outerHTML); } 
 	//need to look for Inline doms to add to dom
