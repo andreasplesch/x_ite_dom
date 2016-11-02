e63014e8fee64f1037974670aa4f3aff006af957
Andreas Plesch <andreasplesch@users.noreply.github.com>
2016-11-02 19:34:20 -0400

    Update cobweb_dom.js

--- cobweb/cobweb_dom.js
+++ cobweb/cobweb_dom.js
@@ -113,9 +113,15 @@ X3D (function (X3DCanvases)
 				ctx. sensor = sensor;*/
 				//only attach callbacks for output fields
 				if (field. isOutput()) // both inputOutput and outputOnly
+				{
 					field .addFieldCallback (
 						"DomIntegration." + field .getName (),
 						this .fieldCallback .bind (null, field, element));
+					if (element. x3d .getBrowser() .trace)
+						field .addFieldCallback (
+							"DomIntegrationTracer." + field .getName (),
+							this .fieldTraceCallback .bind (null, field, element));
+				}		
 			},
 			fieldCallback: function  (field, element, value)
 			{
@@ -140,15 +146,24 @@ X3D (function (X3DCanvases)
 				//event.x3d = sensor.x3d; 
 				element .dispatchEvent (event);
 				//trace to console
-				if (node .getBrowser() .trace)
-				{
-					console .log ( event.timeStamp + 
-						       ": at " + node .getBrowser () .getCurrentTime () +  
-						       " " + node .getTypeName () + 
-						       " '" + node .getName () + "'" +
-						       " " + field .getName () +
-						       ": " + value );
-				}
+// 				if (node .getBrowser() .trace)
+// 				{
+// 					console .log ( event.timeStamp + 
+// 						       ": at " + node .getBrowser () .getCurrentTime () +  
+// 						       " " + node .getTypeName () + 
+// 						       " '" + node .getName () + "'" +
+// 						       " " + field .getName () +
+// 						       ": " + value );
+// 				}
+			},
+			fieldTraceCallback: function  (field, element, value)
+			{
+				console .log ( event.timeStamp + 
+					       ": at " + node .getBrowser () .getCurrentTime () +  
+					       " " + node .getTypeName () + 
+					       " '" + node .getName () + "'" +
+					       " " + field .getName () +
+					       ": " + value );
 			},
 			processRemovedNode: function (element)
 			{	
