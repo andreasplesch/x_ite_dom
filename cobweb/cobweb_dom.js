5cd19f1cee2e88e75f6ee3e389b448c603d42f8d
Andreas Plesch <andreasplesch@users.noreply.github.com>
2016-11-13 21:21:22 -0500

    Update cobweb_dom.js

--- cobweb/cobweb_dom.js
+++ cobweb/cobweb_dom.js
@@ -27,9 +27,20 @@ X3D (function (X3DCanvases)
 			{
 				var scripts = dom .querySelectorAll ('script');
 				for (var i = 0, length = scripts. length; i < length; ++i)
-					
+					this .preprocessScript(scripts[i]);
 				return dom;
-			}
+			},
+			
+			preprocessScript: function (script)
+			{
+				var domParser = new DOMParser();
+				var scriptDoc = domParser .parseFromString (script.textContent, 'application/xml');
+				var fieldDefinitions = scriptDoc .body .children;
+				//comment out field tags
+				for (var i = 0, length = fieldDefinitions .length; i < length; ++i)
+					script.appendChild(fieldDefinitions[0]);
+				// to be continued ...	
+			},
 			
 			setup: function ()
 			{				
@@ -42,7 +53,7 @@ X3D (function (X3DCanvases)
 				//preprocess script nodes if not xhtml
 				
 				if (!documenent.URL.toLowerCase().includes('xhtml'))
-					dom = this .preprocessScripts(dom);
+					this .preprocessScripts(dom);
 	
 				//mybrowser.importDocument(dom); //now also attached x3d property to each node element
 				//update to spec. conforming, latest use
