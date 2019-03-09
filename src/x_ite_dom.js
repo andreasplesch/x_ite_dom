cc87de21067ada1431e98840f95b83a34cc426ea
Holger Seelig <holger.seelig@gmail.com>
2019-03-09 09:56:30 +0100

    Check for routes.
    Optimized X3DRoute and it is no longer of type X3DBaseNode, therefore it must be checked for ROUTE in 'addEventDispatchers'.

--- src/x_ite_dom.js
+++ src/x_ite_dom.js
@@ -13,7 +13,7 @@ X3D (function ()
 	],
 	function (XMLParser)
 	{
-		console .info ("Cobweb XHTML DOM integration enabled");
+		console .info ("X_ITE XHTML DOM integration enabled");
 
 		function DOMIntegration (X3DCanvas)
 		{
@@ -131,7 +131,10 @@ X3D (function ()
 				if (element .x3d === undefined)
 					return;
 	
-				var fields = element .x3d .getFields (); // check for ROUTE ?
+				if (element .nodeName === 'ROUTE')
+					return;
+
+				var fields = element .x3d .getFields ();
 	
 				for (var key in fields) 
 					this .bindFieldCallback (fields [key], element);
