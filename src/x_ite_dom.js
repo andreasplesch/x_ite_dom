9b32326257f9aaf46746eb0bdfe0bd4086bb09c3
Holger Seelig <holger.seelig@gmail.com>
2019-03-14 20:33:21 +0100

    importDocument returns now a Promise as you suggest

--- src/x_ite_dom.js
+++ src/x_ite_dom.js
@@ -56,8 +56,8 @@ X3D (function ()
 					this .preprocessScripts(dom);
 
 				//now also attached x3d property to each node element
-				this .browser .importDocument (dom,
-				function (importedScene)
+				this .browser .importDocument (dom)
+				.then (function (importedScene)
 				{
 					this .browser .replaceWorld (importedScene);
 
@@ -96,7 +96,7 @@ X3D (function ()
 				.bind (this),
 				function (error)
 				{
-					console .log ("Error requiring component libraries", error);
+					console .log ("Error importing document:", error);
 				});
 			},
 			
